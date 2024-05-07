c273f917b3382ce83ab62a8172b779ebbbc69e6a
assarriy <wibowoassariy23@gmail.com>
2024-05-07 10:25:24 +0700

    Adding Login Screen

--- app/src/main/java/com/assarriy/ujianpraktek/OnBoardingActivity.kt
+++ app/src/main/java/com/assarriy/ujianpraktek/OnBoardingActivity.kt
@@ -1,15 +1,30 @@
 package com.assarriy.ujianpraktek
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.assarriy.ujianpraktek.databinding.ActivityOnBoardingBinding
 
-class OnBoardingActivity : AppCompatActivity() {
+class OnBoardingActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityOnBoardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOnBoardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val btnMoveToLogin: Button = findViewById(R.id.onBoardingButton)
+        btnMoveToLogin.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.onBoardingButton -> {
+                val moveIntent = Intent(this@OnBoardingActivity, LoginActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
     }
 }
