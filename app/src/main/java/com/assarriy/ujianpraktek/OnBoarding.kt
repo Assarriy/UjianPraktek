14684a18e310d21770ed1347bf106e060d208991
assarriy <wibowoassariy23@gmail.com>
2024-05-07 09:09:54 +0700

    Binding OnBoardingScreen

--- app/src/main/java/com/assarriy/ujianpraktek/OnBoarding.kt
+++ app/src/main/java/com/assarriy/ujianpraktek/OnBoarding.kt
@@ -2,10 +2,15 @@ package com.assarriy.ujianpraktek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import com.assarriy.ujianpraktek.databinding.ActivityOnBoardingBinding
 
 class OnBoarding : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnBoardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_boarding)
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
